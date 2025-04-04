3984722d7768b10da03303a06b5328d867e4d9c5
Ksieon <keumsiun0503@gmail.com>
2025-04-04 17:39:56 +0900

    :sparkles: chapter_03 업데이트

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Hello from './study01/Hello.jsx'
+import Library from './chapter_03/Library.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <APP />
-    <Hello />
+    <Library />
   </StrictMode>,
 )
