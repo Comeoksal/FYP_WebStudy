5f6d0214ab3a197f001370e6258e200f2cd90a9c
Comeoksal <keumsiun0503@gmail.com>
2025-04-29 14:24:05 +0900

    :sparkles:Feat: server

--- server/main.js
+++ server/main.js
@@ -8,16 +8,12 @@ fastify.get('/', async (req, reply) => {
     return { hello: 'worlds' }
 })
 
-<<<<<<< HEAD
 fastify.get('/sieon', async (req, reply) => {
     return { helqwqwm: 'worsdfnidsfgdgsfgsdggddf' }
 })
 
 const PORT = 5000;
 
-=======
-const PORT = 4000;
->>>>>>> 71e5aae625107c9d26a64a43d5ad2b4aa6ead5fc
 try {
     await fastify.listen({ port: PORT })
     console.log(`Server Running at ${PORT}`);
