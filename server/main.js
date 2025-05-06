aba6ac90f8f8d09bfe9cd7a4c6ce03ddb3f0fbfc
Sieon Keum <keumsiun0503@gmail.com>
2025-05-07 00:25:55 +0900

    :sparkles:Feat: 크롤링

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import axios from 'axios'
+import iconv from 'iconv-lite';
+import { load } from "cheerio";
 
 const fastify = Fastify({
     logger: true
@@ -26,9 +29,34 @@ fastify.post('/api/post', async (req, reply) => {
 
 const PORT = 5000;
 
+const fetchPopularNews = async () => {
+    const response = await axios.get("https://news.naver.com/main/ranking/popularDay.naver", {
+        responseType: "arraybuffer", // 중요: raw binary로 받기
+    });
+
+    // EUC-KR로 디코딩
+    const decoded = iconv.decode(response.data, "EUC-KR");
+    const $ = load(decoded);
+    const articles = [];
+
+    $(".rankingnews_box a").each((i, el) => {
+        const title = $(el).text().trim();
+        const url = $(el).attr("href");
+        if (title && url) {
+            articles.push({
+                title,
+                url: url.startsWith("http") ? url : `https://news.naver.com${url}`,
+            });
+        }
+    });
+
+    return articles;
+};
 try {
     await fastify.listen({ port: PORT })
     console.log(`Server Running at ${PORT}`);
+
+    fetchPopularNews().then(console.log);
 } catch (err) {
     fastify.log.error(err)
     process.exit(1)
